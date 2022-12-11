dfdea8ea8f13c528292d98bc48886e4dc881414d
Davin Singh <davinsinghca@gmail.com>
2022-12-11 13:18:39 -0800

    basic client test

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from dotenv import load_dotenv
+import os
 import discord
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello!')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(TOKEN)
